404c7e67a198b82cd11f7251a395b47ea32d4ba1
FineJadeXavier <FineJadeXavier@gmail.com>
2018-05-09 18:02:15 +0800

    优化

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir xavier_temp && cd xavier_temp
 tee larago <<-'EOF'
 echo "alias laravel='~/.composer/vendor/bin/laravel'" >> ~/.bashrc && echo "alias rm='rm -rf'" >> ~/.bashrc && source ~/.bashrc
 composer global require "laravel/installer"
@@ -9,26 +10,24 @@ mkdir -p /var/www/storage/logs/  &&  touch /var/www/storage/logs/laravel.log && 
 EOF
 
 tee Dockerfile <<-'EOF'
-FROM laravel:temp0
+FROM laravel:temp
 MAINTAINER Xavier <FineJadeXavier@gmail.com>
 RUN cd /home && chmod +x larago
 EXPOSE 80 443 9000 22
 EOF
 
 docker run -d --name laravel_temp registry.cn-hangzhou.aliyuncs.com/xavier/nginx-php-fpm
-docker cp larago laravel_temp:/home/
-docker commit -a 'Xavier' -p -m 'laravel' laravel_temp laravel:temp0
-docker rm -f laravel_temp
-rm larago
 
-docker build -t  laravel:temp1 .
-docker rmi laravel:temp0
-docker run -d -p80:80 --name laravel_temp1 laravel:temp1
+docker cp larago laravel_temp:/home/
+
+docker commit -a 'Xavier' -p -m 'laravel' laravel_temp laravel:temp
+
+docker build -t laravel:temp1 .
+
+docker run -d --name laravel_temp1 laravel:temp1
+
 docker exec -it laravel_temp1 /bin/bash /home/larago
 docker commit -a 'Xavier' -p -m 'laravel'  laravel_temp1 laravel
-docker rm -f laravel_temp1
-sleep 5s
-docker rmi -f laravel:temp1
-rm  Dockerfile
-docker rmi -f registry.cn-hangzhou.aliyuncs.com/xavier/nginx-php-fpm
-rm setup.sh
+docker rm -f laravel_temp laravel_temp1 && docker rmi -f laravel:temp1 laravel:temp registry.cn-hangzhou.aliyuncs.com/xavier/nginx-php-fpm
+rm setup.sh  larago Dockerfile 
+
